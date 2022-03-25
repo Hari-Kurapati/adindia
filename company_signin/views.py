75bfd289b0c328a556424b6fe2b836c0ad1bca91
Rakib Hasan <rakib.cs132@gmail.com>
2022-03-25 22:12:18 +0600

    company sign in added

--- company_signin/views.py
+++ company_signin/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.contrib import messages
+from company_signup.models import Advertisers
 
 # Create your views here.
 def company_signin(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        passw = request.POST.get('password')
+
+        query = f"SELECT * FROM company_signup_advertisers WHERE company_email = '{email}' AND company_password = '{passw}'"
+
+        t = tuple(Advertisers.objects.raw(query))
+
+        if t==():
+            messages.success(request, "Incorrect email address or password")
+        else:
+            return render(request, 'welcome.html')
+
     return render(request, 'company_signin.html')
